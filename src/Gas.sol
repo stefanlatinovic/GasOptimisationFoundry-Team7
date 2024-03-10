5d97585f51c10573ee62b01e4534021a4f8a5eab
Stefan Latinović <stefan.latinovic@outlook.com>
2024-03-10 19:09:25 +0100

    Remove unused contract fields

--- src/Gas.sol
+++ src/Gas.sol
@@ -5,7 +5,6 @@ import "./Ownable.sol";
 
 contract Constants {
     uint256 public tradeFlag = 1;
-    uint256 public basicFlag = 0;
     uint256 public dividendFlag = 1;
 }
 
@@ -15,11 +14,9 @@ contract GasContract is Ownable, Constants {
     mapping(address => uint256) public balances;
     uint256 public tradePercent = 12;
     address public contractOwner;
-    uint256 public tradeMode = 0;
     mapping(address => Payment[]) public payments;
     mapping(address => uint256) public whitelist;
     address[5] public administrators;
-    bool public isReady = false;
     enum PaymentType {
         Unknown,
         BasicPayment,
@@ -27,7 +24,6 @@ contract GasContract is Ownable, Constants {
         Dividend,
         GroupPayment
     }
-    PaymentType constant defaultPayment = PaymentType.Unknown;
 
     History[] public paymentHistory; // when a payment was updated
 
